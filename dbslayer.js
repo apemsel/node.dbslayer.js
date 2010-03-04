dcaa9f5b58360d27b968b8f919b9196cee908b0d
Andy Schuler <andy@leftshoedevelopment.com>
2010-03-03 20:55:04 -0700

    Updated to work with Node.js 0.1.30-27-gb021a84. The promise.emit/addCallback/addErrback event flow changed to events.eventEmitter().emit/addListener, so I updated the connector (dbslayer.js) and the test examples to reflect those changes.

--- dbslayer.js
+++ dbslayer.js
@@ -1,62 +1,76 @@
 /*
 ---
 name: dbslayer.js
-
+ 
 description: Interface to DBSlayer for Node.JS
-
+ 
 author: [Guillermo Rauch](http://devthought.com)
+updated: Andy Schuler (andy at leftshoedevevelopment dot com)
 ...
 */
 
-var sys = require('sys'),
-    http = require('http'),
-    
-    booleanCommands = ['STAT', 'CLIENT_INFO', 'HOST_INFO', 'SERVER_VERSION', 'CLIENT_VERSION'],
+var sys = require('sys');
+var http = require('http');
+var events = require('events');
 
-Server = this.Server = function(host, port, timeout){
+var booleanCommands = ['STAT', 'CLIENT_INFO', 'HOST_INFO', 'SERVER_VERSION', 'CLIENT_VERSION'];
+
+var Server = this.Server = function(host, port, timeout){
   this.host = host || 'localhost';
   this.port = port || 9090;
   this.timeout = timeout;
 };
 
 Server.prototype.fetch = function(object, key){
-  var connection = http.createClient(this.port, this.host),
-      request = connection[connection.get ? 'get' : 'request']('/db?' + escape(JSON.stringify(object)), {'host': this.host}),
-      promise = new process.Promise();
-  
-  promise.timeout(this.timeout);
-
-  request.finish(function(response){
-    response.addListener('body', function(data){  
-      try {
-        var object = JSON.parse(data);
-      } catch(e){
-        return promise.emitError(e);
-      }      
-      
-      if (object.MYSQL_ERROR !== undefined){
-        promise.emitError(object.MYSQL_ERROR, object.MYSQL_ERRNO);
-      } else if (object.ERROR !== undefined){
-        promise.emitError(object.ERROR);
-      } else {
-        promise.emitSuccess(key ? object[key] : object);
-      }      
-    });
-  });
-  
-  return promise;
-};
+  var e = new events.EventEmitter();
+  var connection = http.createClient(this.port, this.host);
+  var request = connection.request("GET",'/db?' + escape(JSON.stringify(object)), {'host': this.host});
+  request.addListener('response',
+    function(response) {
+      var data = [];
+      response.addListener('data',
+        function(chunk) {
+          data.push(chunk);
+        }
+      );
+      response.addListener('end',
+        function() {
+          try {
+            var object = JSON.parse(data.join());
+          }
+          catch(err) {
+            e.emit('error',err);
+            return;
+          }
+          if (object.MYSQL_ERROR !== undefined){
+            e.emit('error', object.MYSQL_ERROR, object.MYSQL_ERRNO);
+            return;
+          } 
+          if (object.ERROR !== undefined){
+            e.emit('error', object.ERROR);
+            return;
+          } 
+          e.emit('success',key ? object[key] : object);
+        }
+      );
+    }
+  );
+  request.close();
+  return e;
+}
 
 Server.prototype.query = function(query){
   return this.fetch({SQL: query}, 'RESULT');
 };
 
 for (var i = 0, l = booleanCommands.length; i < l; i++){
-  Server.prototype[booleanCommands[i].toLowerCase()] = (function(command){
-    return function(){
-      var obj = {};
-      obj[command] = true;
-      return this.fetch(obj, command);
-    };
-  })(booleanCommands[i]);
+  Server.prototype[booleanCommands[i].toLowerCase()] = (
+    function(command){
+      return function(){
+        var obj = {};
+        obj[command] = true;
+        return this.fetch(obj, command);
+      };
+    }
+  )(booleanCommands[i]);
 }
--- test.js
+++ test.js
@@ -1,43 +1,59 @@
 /*
 ---
-name: tools.js
-
+name: test.js
+ 
 description: <
-  This is a demonstration of how dbslayer.js can be used.
-  It takes three parameters from the SQL query, a host
-
+This is a demonstration of how dbslayer.js can be used.
+It takes three parameters from the SQL query, a host
+ 
 author: [Guillermo Rauch](http://devthought.com)
+updated: [Andy Schuler](andy at leftshoedevelopment dot com)
 ...
 */
 
-var sys = require('sys')
-    dbslayer = require('./dbslayer'),    
-    sql = process.ARGV[2],
-    db = new dbslayer.Server();
+var sys = require('sys');
+var dbslayer = require('./dbslayer');
+var sql = process.ARGV[2];
+var db = new dbslayer.Server();
     
 if (!sql){
   sys.puts('Please provide the SQL query');
   return;
 }
 
-db.query(sql)
-  // on success
-  .addCallback(function(result){
-    sys.puts('-------------------------');        
+//basic query
+var query = db.query(sql);
+query.addListener("success", 
+  function(result) {
+    sys.puts('-------------------------');
     for (var i = 0, l = result.ROWS.length; i < l; i++){
       sys.puts('Row ' + i + ': ' + result.ROWS[i].join(' '));
     }
-  })
-  
-  // on error :(
-  .addErrback(function(error, errno){
-    sys.puts('-------------------------');        
-    sys.puts('MySQL error (' + (errno || '') + '): ' + error);
-  });
+  }
+);
 
-['stat', 'client_info', 'host_info', 'server_version', 'client_version'].forEach(function(command){  
-  db[command]().addCallback(function(result){
-    sys.puts('-------------------------');    
-    sys.puts(command.toUpperCase() + ' ' + result);
-  });
-});
+query.addListener("error", 
+  function(error, errno) {
+    sys.puts('-------------------------');
+    sys.puts('MySQL error (' + (errno || '') + '): ' + error);
+  }
+);
+
+['stat', 'client_info', 'host_info', 'server_version', 'client_version'].forEach(
+  function(command){
+    var el = db[command]();
+    el.addListener("success",
+      function(results) {
+        sys.puts('-------------------------');    
+        sys.puts(command.toUpperCase() + ' ' + results);
+      }
+    );
+    
+    el.addListener("error",
+      function(results) {
+        sys.puts('-------------------------');    
+        sys.puts(command.toUpperCase() + ' ' + results);
+      }
+    );
+  }
+);
